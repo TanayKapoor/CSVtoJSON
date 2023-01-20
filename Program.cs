9617cafe3109ad2685113ff84c1849aff502b0a2
Tanay Kapoor <tanay.kapoor@cginfinity.com>
2023-01-20 16:24:47 +0530

    added files

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using CsvToJsonLib;
+
+namespace CsvToJsonProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void start()
+            {
+                Console.WriteLine("\n============\nWelcome! (>_<)\n============\n");
+                Console.WriteLine("Enter the path for the CSV File -");
+                string FP = Console.ReadLine();
+                string FilePath =  $@"{FP}";
+                Processor.ProcessLines(FilePath);
+            }
+
+            void restart()
+            {
+                Console.WriteLine("Press any key to restart...");
+                Console.ReadKey();
+                Console.Clear();
+                start();
+            }
+            try
+            {
+                start();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("File does not exits! Perhaps you might have put wrong filename or don't have extension included!");
+                restart();
+            }
+
+        }
+    }
+}
